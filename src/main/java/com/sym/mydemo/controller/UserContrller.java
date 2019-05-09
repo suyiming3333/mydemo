d4ec9cbae983588f27b247e2b6691c91ae074c3d
suyiming <suyiming3333@gmail.com>
2019-05-09 23:48:10 +0800

    引入是spring security 以及thmylete

---
+++ src/main/java/com/sym/mydemo/controller/UserContrller.java
@@ -0,0 +1,24 @@
+package com.sym.mydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author suyiming3333@gmail.com
+ * @version V1.0
+ * @Title: DemoContrller
+ * @Package com.sym.mydemo.controller
+ * @Description: TODO
+ * @date 2019/5/9 23:18
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserContrller {
+    @RequestMapping("/index")
+    public String themaleaf(Model model) {
+//        model.addAttribute("name", "Joshua");
+        return "/user/index";
+    }
+}
---
+++ src/main/java/com/sym/mydemo/security/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sym.mydemo.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author suyiming3333@gmail.com
+ * @version V1.0
+ * @Title: SecurityConfig
+ * @Package com.sym.mydemo.security.config
+ * @Description: TODO
+ * @date 2019/5/9 22:45
+ */
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**","index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
